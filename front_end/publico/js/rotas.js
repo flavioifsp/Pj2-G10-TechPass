8101b533e9c34fcb783abb58f9df99704120e625
leonardo <leonardolfelix12@gmail.com>
2023-09-23 01:31:06 -0300

    att rotas (funcional, mas nem tanto)

--- front_end/publico/js/rotas.js
+++ front_end/publico/js/rotas.js
@@ -6,54 +6,40 @@
 
 const linha = (num, bairroI, bairroEnd, horarioI, horarioEnd, ItinerarioI, ItinerarioEnd, divpai) => {
     const molde = document.createElement("div")
+    const moldemodal1 = document.createElement("div")
+    const moldemodal2 = document.createElement("div")
     const idmodal1 = `L${num}1`
     const idmodal2 = `L${num}2`
     const horasI = `horasI${num}`
     const horasEnd = `horasEnd${num}`
     const itiI = `itiI${num}`
     const itiEnd = `itiEnd${num}`
-    const btnalternar1 = `btnal${idmodal1}`
-    const btnalternar2 = `btnal${idmodal2}`
     bairroEnd = bairroEnd.toUpperCase()
     bairroI = bairroI.toUpperCase()
 
  
 
     // transformar a string em uma array separando por "," > cd vetor do array será transformado em um elemento (li) e dps sera colocada em uma lista(ul)
-    const addLista = (string, lista, rounded) => {
+    const addLista = (string, lista, rounded, modal) => {
         string = string.trim().split(",")
         
         for(x of string){
             const substring = document.createElement("li")
             substring.classList.add("list-group-item", rounded, "fs-4")
             substring.innerHTML = x
-            molde.querySelector("#"+lista).appendChild(substring)
+            modal.querySelector("#"+lista).appendChild(substring)
         }
 
     }
 
 
     
-    function alternarmodal(btn, modal, modalfuturo, a){        
-        modal = a.querySelector("#"+modal)
-        modalfuturo = a.querySelector("#"+modalfuturo)
-        btn = a.querySelector("#"+btn)
-        
-        const myModal = new bootstrap.Modal(modal)
-        const myModalfuturo = new bootstrap.Modal(modalfuturo)
-        
-        console.log(modal.querySelector(btn) )
-        modal.querySelector(btn).addEventListener("click", (elem) => {
-            myModal.hide()
-            document.querySelector("modal-backdrop").remove()
-            myModalfuturo.show()
-        })
-    }
+ 
+  
 
     // o molde da lista em html e css
     molde.innerHTML = ` 
-        <div class="bg-dark-subtle rounded-4 d-flex flex-column  flex-lg-row px-3" data-bs-toggle="modal"
-        data-bs-target="#${idmodal1}" style="cursor: pointer; height: 70px;">
+        <div class="bg-dark-subtle rounded-4 d-flex flex-column  flex-lg-row px-3" data-bs-target="#${idmodal1}" data-bs-toggle="modal" style="cursor: pointer; height: 70px;">
         <div class="d-flex align-items-center column-gap-md-3 column-gap-2 py-2">
             <img src="./img/icon bus.png" alt="..." class="img-fluid" style="width: 12%" />
 
@@ -63,9 +49,12 @@ const linha = (num, bairroI, bairroEnd, horarioI, horarioEnd, ItinerarioI, Itine
             </div>
         </div>
         </div>
-
-        <div class="modal fade " id="${idmodal1}" tabindex="-1" aria-labelledby="${idmodal1}ModalLabel"
-        aria-hidden="true">
+    `   
+   
+    moldemodal1.classList.add("modal", "fade")
+    moldemodal1.id = idmodal1
+    moldemodal1.style="background-color: rgba(0, 0, 0, 0.5);"
+    moldemodal1.innerHTML = `
         <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
             <div class="modal-content">
             <div class="modal-header px-4">
@@ -75,7 +64,6 @@ const linha = (num, bairroI, bairroEnd, horarioI, horarioEnd, ItinerarioI, Itine
             <div class="modal-body px-4  ">
 
                 <div class="container-fluid p-0 mx-auto row">
-
 
                 <!--bairro inicio -->
                 <ul class="list-group  col text-center pe-0" id="${horasI}">
@@ -94,17 +82,21 @@ const linha = (num, bairroI, bairroEnd, horarioI, horarioEnd, ItinerarioI, Itine
 
             </div>
             <div class="modal-footer border-0 px-4">
-                <button type="button" class="btn btn-primary btnalternar" id="${btnalternar1}">Itinerario</button>
+                <button type="button" class="btn btn-primary btnalternar" data-bs-target="#${idmodal2}" id="aaaaaaa" data-bs-toggle="modal">Itinerario</button>
 
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
             </div>
             </div>
         </div>
-        </div>
+    `
+    const myModal = new bootstrap.Modal(moldemodal1, {
+        backdrop: false
+      })    
 
-
-        <div class="modal fade " id="${idmodal2}" tabindex="-1" aria-labelledby="a"
-        aria-hidden="true">
+    moldemodal2.classList.add("modal", "fade")
+    moldemodal2.id = idmodal2
+    moldemodal2.style="background-color: rgba(0, 0, 0, 0.5);"
+    moldemodal2.innerHTML = `
         <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
             <div class="modal-content">
             <div class="modal-header px-4">
@@ -120,41 +112,44 @@ const linha = (num, bairroI, bairroEnd, horarioI, horarioEnd, ItinerarioI, Itine
                 <ul class="list-group  col  pe-0" id="${itiI}">
 
                     <li class="list-group-item rounded-end-0 fs-3">${bairroI}</li>
-                   
+                
                 </ul>
 
                 <!--bairro fim -->
                 <ul class="list-group  col  ps-0" id="${itiEnd}">
                     <li class="list-group-item rounded-start-0 fs-3">${bairroEnd}</li>
-                   
+                
                 </ul>
 
                 </div>
 
             </div>
             <div class="modal-footer border-0 px-4">
-                <button  type="button" class="btn btn-primary " id="${btnalternar2}">Horario</button>
+                <button  type="button" class="btn btn-primary " data-bs-target="#${idmodal1}"  data-bs-toggle="modal">Horario</button>
 
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
             </div>
             </div>
         </div>
-        </div>
-    `   
+    `
+    const myModal2 = new bootstrap.Modal(moldemodal2, {
+            backdrop: false
+        })    
     
-
     
-
-    alternarmodal(btnalternar2, idmodal1, idmodal2, molde)
-
-    addLista(horarioI, horasI, "rounded-end-0")
-    addLista(horarioEnd, horasEnd, "rounded-start-0")
+    addLista(horarioI, horasI, "rounded-end-0", moldemodal1)
+    addLista(horarioEnd, horasEnd, "rounded-start-0", moldemodal1)
     
-    addLista(ItinerarioI, itiI, "rounded-end-0")
-    addLista(ItinerarioEnd, itiEnd, "rounded-start-0")
+    addLista(ItinerarioI, itiI, "rounded-end-0", moldemodal2)
+    addLista(ItinerarioEnd, itiEnd, "rounded-start-0", moldemodal2)
 
 
     document.querySelector(divpai).appendChild(molde)
+    document.body.appendChild(moldemodal1)
+    document.body.appendChild(moldemodal2)
+
+   
+   
 }
 
 
@@ -164,13 +159,13 @@ linha(
 // bairro incial e bairro final
 "bairro paia", "paioso", 
 // horario do bairro incial 
-"11:11, 11:11, 11:11, 11:11", 
+"11:11, 11:11, 11:101, 11:11", 
 // horario do bairro final
-"11:11, 11:11, 11:11, 11:11", 
+"11:11, 11:101, 11:11, 11:11", 
 // itinerario do bairro inicial
-"paia, paia",
+"paia, pakhjjkhia",
 // itinerario do bairro final
-"paia, paia",
+"paia, paijkhka",
 // div em q estára
 "#lista-linha"
 )
