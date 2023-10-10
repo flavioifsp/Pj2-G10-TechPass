8abdaa6053b1a90088a6ba7dc11fe6f4377ee852
leonardo <leonardolfelix12@gmail.com>
2023-10-10 17:58:38 -0300

    att user front p2

--- frontend/routes/user.js
+++ frontend/routes/user.js
@@ -1,20 +1,55 @@
 var express = require('express');
 var router = express.Router();
 
-const menu = [
-    {href: "/", icon: "bi bi-wallet2", text: "MEUS CARTÕES", active:""},
-    {href: "/", icon: "bi bi-credit-card-2-back", text: "FORMAS DE PAGAMENTO", active:""}
-]; 
-const acti = (tudo) => {
-    menu.push(tudo)
-    menu[menu.length].active = "active"
-}
 
-/* GET User. */
-router.get( '/', function(req, res, next) {
-    acti({href: "/", icon: "bi bi-person-circle", text: "MEU PERFIL", active:""})
-    res.render( 'site_publico/pages/user/MeuPerfil.ejs' , { title: 'Perfil', layout: 'site_publico/layouts/layout_user.ejs', menu});
-});
+class Menu{
+    constructor(){
+        this.array = []   
+    }
+    
+    
+
+    addpag(endereco, href, icon, text){
+        const hrefM = href
+        const indi = this.array.length
+        const menu = this.array
+        menu.push({ href:hrefM, icon, text, active: ""})
+
+
+
+        router.get(href, function(req, res, next) {
+            console.log(menu)
+            menu[indi].active = "active"
+            res.render( endereco , { title: 'Perfil', layout: 'site_publico/layouts/layout_user.ejs', menu});
+        });
+    }
+
+} 
+
+
+const elemenu = new Menu()
+
+elemenu.addpag("site_publico/pages/user/MeuPerfil.ejs", "/", "bi bi-person-circle", "MEU PERFIL")
+elemenu.addpag("site_publico/pages/user/formadepag.ejs","/formasdepagamento", "bi bi-wallet2", "FORMAS DE PAGAMENTO")
+// elemenu.addpag("site_publico/pages/user/MeuPerfil.ejs","/", "bi bi-credit-card-2-back", "FORMAS DE PAGAMENTO")
+
+
+   
+
+
+// /* GET User. */
+// router.get( '/', function(req, res, next) {
+//     const menu = elemenu.lista()
+//     elemenu.active(0)
+//     res.render( 'site_publico/pages/user/MeuPerfil.ejs' , { title: 'Perfil', layout: 'site_publico/layouts/layout_user.ejs', menu});
+// });
+
+// /* GET formas de pagamento. */
+// router.get( '/formasdepagamento', function(req, res, next) {
+//     const menu = elemenu.lista()
+//     elemenu.active(1)
+//     res.render( 'site_publico/pages/user/MeuPerfil.ejs' , { title: 'Perfil', layout: 'site_publico/layouts/layout_user.ejs', menu});
+// });
 
 
 module.exports = router;
