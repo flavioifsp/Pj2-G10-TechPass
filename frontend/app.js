80fe441c69c8bd10bfe59b43ef2ee715b1343998
leonardo <leonardolfelix12@gmail.com>
2023-10-16 14:16:23 -0300

    exportando script rotas para ejs

--- frontend/app.js
+++ frontend/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+ 
 
 app.use('/', indexRouter);
 app.use('/user', userRouter);
@@ -41,8 +41,8 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  res.status(err.status || 500); 
   res.render('error');
 });
-
+ 
 module.exports = app;
