557d60e35d66a16b52804de8b3b82f555422f0de
leonardo <leonardolfelix12@gmail.com>
2023-10-21 21:46:57 -0300

    metodo para alternar entre pags concluido

--- frontend/public/js/TrocaDePag.js
+++ frontend/public/js/TrocaDePag.js
@@ -1,14 +1,19 @@
-function AlternarDePag(btn, pag, proxima_subpag) {
-    document.addEventListener("DOMContentLoaded", () => {
 
-        document.querySelector("#"+btn).addEventListener("click", async () => {
-            try {
-                let subpag = (await axios.get(`http://localhost:3000/user/${pag}/subpags/${proxima_subpag}`)).data
-                
-                document.querySelector("#info").innerHTML  = subpag
-            } catch (erro) {
-                acionarerro(erro, erro.status)
+function AlternarDePag(btn, pag, proxima_subpag) {
+
+    document.querySelector("#" + btn).addEventListener("click", async () => {
+        try {
+            let subpag = (await axios.get(`http://localhost:3000/user/${pag}/subpags/${proxima_subpag}`)).data
+            const divpai = document.querySelector("#info")
+
+            divpai.innerHTML = subpag
+            
+            for( elem of [...divpai.querySelectorAll('script')]) {
+                eval(elem.innerText);
             }
-        })
+            
+        } catch (erro) {
+            acionarerro(erro, erro.status)
+        }
     })
 }
