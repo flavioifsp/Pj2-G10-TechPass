f59c40472e0583285e443bb25c2f24671a9ad606
leo <leonardolfelix12@gmail.com>
2023-11-10 15:24:06 -0300

    melhorando o script

--- abrixamp.bat
+++ abrixamp.bat
@@ -1,2 +1,2 @@
 @echo off
-start /B /MIN ""  "C:\xampp\mysql\bin\mysqld.exe" --defaults-file="C:\xampp\mysql\bin\my.ini" --standalone --console
+start /B  ""  "C:\xampp\mysql\bin\mysqld.exe" --defaults-file="C:\xampp\mysql\bin\my.ini" --standalone --console
