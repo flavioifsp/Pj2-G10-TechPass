ab24bf9ff786624c29d4ccb6a51f8851177f6c35
leonardo <leonardolfelix12@gmail.com>
2023-11-21 06:02:44 -0300

    crud de motorista feito

--- backend/routes/linhas.js
+++ backend/routes/linhas.js
@@ -5,92 +5,37 @@ const prisma = new (require("@prisma/client").PrismaClient)();
 const exception = require("../js/erro");
 
 /* retorna todas as linhas. */
-router.get("/allLinhas", async function (req, res, next) {
+router.get("/", async function (req, res, next) {
   try {
-    // const linhas = await prisma.onibus.findMany()
+    let linhas = await prisma.linhas.findMany({
 
-    // criando o array
-    const linhas = [];
-
-    // paia deveria ser o array com as infos que vc pegou do banco
-    //renomeie os nomes dps do "elem." para se adequar as infos do array do banco de dados
-    const paia = [
-      {
-        bairro1: "olaria",
-        bairro2: "tinga",
-        num_linha: 100,
-        horario_b1: [
-          "11:111",
-          "11:11",
-          "11:11",
-          "11:11",
-          "11:11",
-          "11:11",
-          "11:11",
-          "11:11",
-          "11:11",
-          "11:11",
-          "11:11",
-        ],
-        horario_b2: [
-          "11. :11",
-          "11:11",
-          "11:11",
-          "11:11",
-          "11:11",
-          "11:11",
-          "11:11",
-          "11:11",
-          "11:11",
-          "11:11",
-          "11:11",
-        ],
-        itinerario_b1: [
-          "kaSdnfjkaslfnojksdnfjs",
-          "kaSdnfjkaslfnojksdnfjs",
-          "kaSdnfjkaslfnojksdnfjs",
-          "kaSdnfjkaslfnojksdnfjs",
-          "kaSdnfjkaslfnojksdnfjs",
-          "kaSdnfjkaslfnojksdnfjs",
-          "kaSdnfjkaslfnojksdnfjs",
-          "kaSdnfjkaslfnojksdnfjs",
-          "kaSdnfjkaslfnojksdnfjs",
-        ],
-        itinerario_b2: [
-          "kaSdnfjkaslfnojksdnfjs",
-          "kaSdnfjkaslfnojksdnfjs",
-          "kaSdnfjkaslfnojksdnfjs",
-          "kaSdnfjkaslfnojksdnfjs",
-          "kaSdnfjkaslfnojksdnfjs",
-          "kaSdnfjkaslfnojksdnfjs",
-          "kaSdnfjkaslfnojksdnfjs",
-          "kaSdnfjkaslfnojksdnfjs",
-          "kaSdnfjkaslfnojksdnfjs",
-        ],
-      },
-    ];
-
-    // colocando as informações no molde
-    // o ".push()" está inserindo aquele objeto ao array linhas
-    paia.map((elem) => {
-      linhas.push({
-        bairro1: elem.bairro1,
-        bairro2: elem.bairro2,
-        numLinha: elem.num_linha,
-        infos_da_linha: [
-          {
-            infosB1: elem.horario_b1,
-            infosB2: elem.horario_b2,
-          },
-          {
-            infosB1: elem.itinerario_b1,
-            infosB2: elem.itinerario_b2,
-          },
-        ],
-      });
+      
     });
 
-    res.send(JSON.stringify(linhas));
+    // const temp = []
+    // linhas.forEach((e) =>{
+    //   e
+    // })
+
+    // linhas = linhas.map((elem) => {
+    //   return {
+    //     bairro1: linhas.,
+    //     bairro2: elem.bairro2,
+    //     numLinha: elem.num_linha,
+    //     infos_da_linha: [
+    //       {
+    //         infosB1: elem.horario_b1,
+    //         infosB2: elem.horario_b2,
+    //       },
+    //       {
+    //         infosB1: elem.itinerario_b1,
+    //         infosB2: elem.itinerario_b2,
+    //       },
+    //     ],
+    //   };
+    // });
+
+    res.json(linhas);
   } catch (er) {
     const erro = exception(er);
     res.status(erro.code).send(erro.msg);
@@ -111,8 +56,8 @@ router.post("/", async (req, res, next) => {
     //
     // };
 
-    let numeroDalinha
-  
+    let numeroDalinha;
+
     for (const rota of [rota1, rota2]) {
       const { bairroDestino, bairroOrigem, percursos, horarios } = rota;
 
@@ -130,9 +75,9 @@ router.post("/", async (req, res, next) => {
             create: horarios,
           },
         },
-        select:{
-          id: true
-        }
+        select: {
+          id: true,
+        },
       });
     }
 
