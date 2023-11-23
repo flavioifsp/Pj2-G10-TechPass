52c4e04fd2f40486798226fea6c47b0f45eb95ca
leonardo <leonardolfelix12@gmail.com>
2023-11-23 00:26:26 -0300

    fazendo modals do rotas&linhas

--- frontend/public/js/criarFuncaoModal.js
+++ frontend/public/js/criarFuncaoModal.js
@@ -1,4 +1,4 @@
-function criarFuncaoModal(ModalHtml, callback) {
+function criarFuncaoModal(ModalHtml, callback = () => {}) {
   return function (objEjs) {
     const umadiv = document.createElement("div");
 
@@ -12,10 +12,26 @@ function criarFuncaoModal(ModalHtml, callback) {
 
     const InstanciaBoostrapDoModal = new bootstrap.Modal(modal);
 
-    InstanciaBoostrapDoModal.toggle();
+    const modalspag = document.querySelectorAll(".modal");
+    modalspag.forEach((e, i) => {
+      const modal0 = new bootstrap.Modal(e);
 
-    modal.addEventListener("hidden.bs.modal", () => {
-      InstanciaBoostrapDoModal.dispose();
+      if (e.classList.contains("show")) {
+        modal0.hide();
+        e.addEventListener("hidden.bs.modal", function paia() {
+          InstanciaBoostrapDoModal.toggle();
+          e.removeEventListener(paia);
+        });
+      } else if (modalspag.length - 1 == i) {
+        InstanciaBoostrapDoModal.toggle();
+      }
     });
+
+    modal.addEventListener(
+      "hidden.bs.modal",
+      (abb = () => {
+        modal.remove();
+      })
+    );
   };
 }
