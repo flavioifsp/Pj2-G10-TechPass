5719459f444d8dd71dba06008a68a70255ef3f60
flavio <l.flavio@aluno.ifsp.edu.br>
2023-12-12 14:10:48 -0300

    #

--- backend/addViagem.js
+++ backend/addViagem.js
@@ -1,66 +1,34 @@
 const prisma = new (require("@prisma/client").PrismaClient)();
 
-const mockPassageiros = [
-  
-    {
-      placa: "ABC-1234",
-      estado_atual: "Em operação",
-      possui_acessibilidade: "sim",
-      quantidade_passageiros: 30,
-    },
-    {
-      placa: "DEF-5678",
-      estado_atual: "Em manutenção",
-      possui_acessibilidade: "nao",
-      quantidade_passageiros: 0,
-    },
-    {
-      placa: "GHI-9012",
-      estado_atual: "Em operação",
-      possui_acessibilidade: "sim",
-      quantidade_passageiros: 20,
-    },
-    {
-      placa: "JKL-3456",
-      estado_atual: "Em operação",
-      possui_acessibilidade: "sim",
-      quantidade_passageiros: 10,
-    },
-    {
-      placa: "MNO-7890",
-      estado_atual: "Em manutenção",
-      possui_acessibilidade: "sim",
-      quantidade_passageiros: 0,
-    },
-    {
-      placa: "PQR-4567",
-      estado_atual: "Em operação",
-      possui_acessibilidade: "nao",
-      quantidade_passageiros: 25,
-    },
-    {
-      placa: "STU-9012",
-      estado_atual: "Em operação",
-      possui_acessibilidade: "sim",
-      quantidade_passageiros: 40,
-    },
-    {
-      placa: "VWX-3456",
-      estado_atual: "Em operação",
-      possui_acessibilidade: "nao",
-      quantidade_passageiros: 15,
-    },
-    {
-      placa: "YZ1-7890",
-      estado_atual: "Em manutenção",
-      possui_acessibilidade: "sim",
-      quantidade_passageiros: 0,
-    },
-  
-];
+const onibus_id = 1;
+const motorista_SU_id = 81;
+const linhas_id = 2;
 
-mockPassageiros.forEach(async (elem) => {
-  const response = await prisma.onibus.create({
-    data: elem,
-  });
+const paia = (async () => {
+try {
+const response = await prisma.viagem.create({
+data: {
+onibus_id,
+motorista_SU_id,
+linhas_id,
+},
+include: {
+linhas: true,
+motorista: {
+select: {
+superuser: {
+select: {
+nome: true,
+},
+},
+},
+},
+},
 });
+
+console.log("linha: " + response.linhas.numero_linha);
+console.log("motorista: " + response.motorista.superuser.nome);
+} catch (error) {
+console.log(error);
+}
+})();
