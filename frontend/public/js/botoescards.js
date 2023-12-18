976cbe26959e0b8c569bd1dc5de5b05b555d7b51
leonardo <leonardolfelix12@gmail.com>
2023-12-18 13:40:18 -0300

    carrosel do index melhorado

--- frontend/public/js/botoescards.js
+++ frontend/public/js/botoescards.js
@@ -1,14 +1,5 @@
 const btns = [...document.querySelectorAll(".cor-botoes")];
 
-const shadow = document.createElement("div");
-shadow.style.backgroundColor = "#b7b7b7";
-shadow.style.height = "100%"
-shadow.style.width = "100%"
-shadow.style.position = "absolute"
-shadow.style.top = "0px"
-shadow.style.transition = "all 1s ease-in-out"
-shadow.style.zIndex = "-1"
-
 btns.forEach((e) => {
   let btnOescolhido;
   e.addEventListener("click", (event) => {
@@ -17,15 +8,14 @@ btns.forEach((e) => {
     for (x of btns) {
       x.style.background =
         "linear-gradient(45deg, rgba(0, 117, 255, 0.7978737027037377) 25%, rgba(96, 2, 255, 0.6970333665692839) 76%)";
+      x.parentNode.style.background = "";
 
       if (x === btnOescolhido) {
         btnOescolhido.style.background = "#6000CF";
 
-        const divPai = btnOescolhido.parentNode
-
-        divPai.style.position = "relative"
-
-        divPai.appendChild(shadow)
+        btnOescolhido.parentNode.appendChild(
+          document.querySelector("#backgroundPaioso")
+        );
       }
     }
   });
