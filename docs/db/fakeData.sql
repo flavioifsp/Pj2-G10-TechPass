ca7623fda5f80c0057e4fc6a3daf6eb469b0aa27
leonardo <leonardolfelix12@gmail.com>
2024-11-22 22:33:21 -0300

    chore(setup): melhorando os dados fake

--- docs/db/fakeData.sql
+++ docs/db/fakeData.sql
@@ -1,16 +1,16 @@
--- MySQL dump 10.13  Distrib 8.0.32, for Linux (x86_64)
+/*M!999999\- enable the sandbox mode */
+-- MariaDB dump 10.19  Distrib 10.11.10-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: techpassdb
 -- ------------------------------------------------------
--- Server version	8.0.32
-
+-- Server version	10.11.10-MariaDB-ubu2204
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
 ;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
 ;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
 ;
-/*!50503 SET NAMES utf8mb4 */
+/*!40101 SET NAMES utf8mb4 */
 ;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */
 ;
@@ -29,7 +29,7 @@
 -- Current Database: `techpassdb`
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `techpassdb` /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `techpassdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci */;
 
 USE `techpassdb`;
 
@@ -40,12 +40,12 @@ USE `techpassdb`;
 DROP TABLE IF EXISTS `adm`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */
 ;
-/*!50503 SET character_set_client = utf8mb4 */
+/*!40101 SET character_set_client = utf8 */
 ;
 
 CREATE TABLE `adm` (
-    `superUser_id` int unsigned NOT NULL AUTO_INCREMENT,
-    `cargo` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `superUser_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `cargo` varchar(45) DEFAULT NULL,
     PRIMARY KEY (`superUser_id`),
     KEY `fk_ADM_superUser1_idx` (`superUser_id`),
     CONSTRAINT `fk_ADM_superUser1` FOREIGN KEY (`superUser_id`) REFERENCES `superuser` (`id`) ON DELETE CASCADE
@@ -74,15 +74,15 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `atendente`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */
 ;
-/*!50503 SET character_set_client = utf8mb4 */
+/*!40101 SET character_set_client = utf8 */
 ;
 
 CREATE TABLE `atendente` (
-    `superUser_id` int unsigned NOT NULL AUTO_INCREMENT,
-    `local_de_trabalho_id` int DEFAULT NULL,
-    `turno` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `telefone` varchar(13) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `endereco` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `superUser_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `local_de_trabalho_id` int(11) DEFAULT NULL,
+    `turno` varchar(45) DEFAULT NULL,
+    `telefone` varchar(13) DEFAULT NULL,
+    `endereco` varchar(255) DEFAULT NULL,
     PRIMARY KEY (`superUser_id`),
     UNIQUE KEY `superUser_id_UNIQUE` (`superUser_id`),
     KEY `fk_atendente_loja_recarga1_idx` (`local_de_trabalho_id`),
@@ -105,14 +105,14 @@ INSERT INTO
     `atendente`
 VALUES (
         1,
-        1,
+        NULL,
         'manha',
         '11956774880',
         'Rua Ruy Barbosa 519'
     ),
     (
         2,
-        2,
+        NULL,
         'manha',
         '11956774880',
         'Rua Ruy Barbosa 519'
@@ -129,21 +129,21 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `cartoes_do_cliente`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */
 ;
-/*!50503 SET character_set_client = utf8mb4 */
+/*!40101 SET character_set_client = utf8 */
 ;
 
 CREATE TABLE `cartoes_do_cliente` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `codigo_do_cartao` int DEFAULT NULL,
-    `clientes_id` int NOT NULL,
-    `cartao_id` int NOT NULL,
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `codigo_do_cartao` int(11) DEFAULT NULL,
+    `clientes_id` int(11) NOT NULL,
+    `cartao_id` int(11) NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `codigo_do_cartao_UNIQUE` (`codigo_do_cartao`),
     KEY `fk_clientes_has_cartao_passe_clientes` (`clientes_id`),
     KEY `fk_clliente_tem_cartao_cartao1_idx` (`cartao_id`),
     CONSTRAINT `fk_clientes_has_cartao_passe_clientes` FOREIGN KEY (`clientes_id`) REFERENCES `clientes` (`id`) ON DELETE CASCADE,
     CONSTRAINT `fk_clliente_tem_cartao_cartao1` FOREIGN KEY (`cartao_id`) REFERENCES `tipos_de_cartao` (`id`) ON DELETE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */
 ;
 
@@ -157,8 +157,7 @@ LOCK TABLES `cartoes_do_cliente` WRITE;
 
 INSERT INTO
     `cartoes_do_cliente`
-VALUES (1, NULL, 8, 1),
-    (2, 3344440, 10, 3),
+VALUES (2, 3344440, 10, 3),
     (3, 9452789, 3, 2),
     (4, 1234, 8, 1);
 /*!40000 ALTER TABLE `cartoes_do_cliente` ENABLE KEYS */
@@ -173,18 +172,18 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `clientes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */
 ;
-/*!50503 SET character_set_client = utf8mb4 */
+/*!40101 SET character_set_client = utf8 */
 ;
 
 CREATE TABLE `clientes` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `email` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `cpf` varchar(14) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `username` varchar(90) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `senha` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `nome` varchar(120) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `email` varchar(64) NOT NULL,
+    `cpf` varchar(14) NOT NULL,
+    `username` varchar(90) DEFAULT NULL,
+    `senha` varchar(200) DEFAULT NULL,
+    `nome` varchar(120) DEFAULT NULL,
     `nascimento` date DEFAULT NULL,
-    `saldo` decimal(6, 2) DEFAULT '0.00',
+    `saldo` decimal(6, 2) DEFAULT 0.00,
     PRIMARY KEY (`id`),
     UNIQUE KEY `email_UNIQUE` (`email`),
     UNIQUE KEY `cpf_UNIQUE` (`cpf`)
@@ -280,7 +279,7 @@ VALUES (
         '$2a$10$Zm4E/x.I75/y.kUwHfEuAu3SJx4233LGAgx0oXTVJARKEaLwiDk5y',
         'vitoria ',
         '1995-04-03',
-        15.00
+        22.00
     ),
     (
         9,
@@ -314,21 +313,21 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `embarque`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */
 ;
-/*!50503 SET character_set_client = utf8mb4 */
+/*!40101 SET character_set_client = utf8 */
 ;
 
 CREATE TABLE `embarque` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `cartaoDoCliente_id` int DEFAULT NULL,
-    `viagem_id` int NOT NULL,
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `cartaoDoCliente_id` int(11) DEFAULT NULL,
+    `viagem_id` int(11) NOT NULL,
     `historico_tarifa` decimal(6, 2) DEFAULT NULL,
-    `data` datetime DEFAULT CURRENT_TIMESTAMP,
+    `data` datetime DEFAULT current_timestamp(),
     PRIMARY KEY (`id`),
     KEY `fk_viagem_has_cliente_has_cartao_passe_viagem1` (`viagem_id`),
     KEY `fk_embarque_cartoes_do_cliente1_idx` (`cartaoDoCliente_id`),
     CONSTRAINT `fk_embarque_cartoes_do_cliente1` FOREIGN KEY (`cartaoDoCliente_id`) REFERENCES `cartoes_do_cliente` (`id`) ON DELETE SET NULL,
     CONSTRAINT `fk_viagem_has_cliente_has_cartao_passe_viagem1` FOREIGN KEY (`viagem_id`) REFERENCES `viagem` (`id`) ON DELETE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+) ENGINE = InnoDB AUTO_INCREMENT = 11 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */
 ;
 
@@ -362,6 +361,55 @@ VALUES (
         1,
         5.00,
         '2024-11-19 16:03:39'
+    ),
+    (
+        4,
+        4,
+        2,
+        4.00,
+        '2024-11-22 22:47:07'
+    ),
+    (
+        5,
+        4,
+        3,
+        4.00,
+        '2024-11-22 22:51:41'
+    ),
+    (
+        6,
+        4,
+        3,
+        4.00,
+        '2024-11-22 22:51:47'
+    ),
+    (
+        7,
+        4,
+        4,
+        4.00,
+        '2024-11-22 22:54:13'
+    ),
+    (
+        8,
+        4,
+        4,
+        4.00,
+        '2024-11-22 22:54:23'
+    ),
+    (
+        9,
+        4,
+        5,
+        4.00,
+        '2024-11-22 22:56:24'
+    ),
+    (
+        10,
+        4,
+        6,
+        4.00,
+        '2024-11-22 22:58:19'
     );
 /*!40000 ALTER TABLE `embarque` ENABLE KEYS */
 ;
@@ -375,14 +423,14 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `horario_diario_saida`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */
 ;
-/*!50503 SET character_set_client = utf8mb4 */
+/*!40101 SET character_set_client = utf8 */
 ;
 
 CREATE TABLE `horario_diario_saida` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `linhas_id` int unsigned NOT NULL,
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `linhas_id` int(10) unsigned NOT NULL,
     `horario_de_saida` time NOT NULL,
-    `duracaoEstimada` int DEFAULT NULL,
+    `duracaoEstimada` int(11) DEFAULT NULL,
     PRIMARY KEY (`id`),
     KEY `fk_horario_diario_saida_linhas1_idx` (`linhas_id`),
     CONSTRAINT `fk_horario_diario_saida_linhas1` FOREIGN KEY (`linhas_id`) REFERENCES `linhas` (`id`) ON DELETE CASCADE
@@ -426,14 +474,14 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `linhas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */
 ;
-/*!50503 SET character_set_client = utf8mb4 */
+/*!40101 SET character_set_client = utf8 */
 ;
 
 CREATE TABLE `linhas` (
-    `id` int unsigned NOT NULL AUTO_INCREMENT,
-    `numero_linha` int NOT NULL,
-    `bairroOrigem` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `bairroDestino` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `numero_linha` int(11) NOT NULL,
+    `bairroOrigem` varchar(45) NOT NULL,
+    `bairroDestino` varchar(45) NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB AUTO_INCREMENT = 17 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */
@@ -467,18 +515,18 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `loja_recarga`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */
 ;
-/*!50503 SET character_set_client = utf8mb4 */
+/*!40101 SET character_set_client = utf8 */
 ;
 
 CREATE TABLE `loja_recarga` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `nome` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT 'Tech Pass',
-    `cep` varchar(9) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `endereco` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `nome` varchar(45) DEFAULT 'Tech Pass',
+    `cep` varchar(9) DEFAULT NULL,
+    `endereco` varchar(255) DEFAULT NULL,
     `lat` decimal(9, 6) DEFAULT NULL,
     `lng` decimal(9, 6) DEFAULT NULL,
     PRIMARY KEY (`id`)
-) ENGINE = InnoDB AUTO_INCREMENT = 4 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+) ENGINE = InnoDB AUTO_INCREMENT = 8 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */
 ;
 
@@ -493,28 +541,28 @@ LOCK TABLES `loja_recarga` WRITE;
 INSERT INTO
     `loja_recarga`
 VALUES (
-        1,
-        'primeria',
-        '116652-85',
-        'Avenida Brasília, Indaiá, Caraguatatuba, SP',
-        -23.631863,
-        -45.431446
+        4,
+        'Jardim',
+        '116632-00',
+        'Avenida Ipiranga, Jardim Casa Branca, Caraguatatuba, SP',
+        -23.616264,
+        -45.380836
     ),
     (
-        2,
-        'segunda',
-        '078500-40',
-        'Rua Ruy Barbosa, Vila Zanela, Franco da Rocha, SP',
-        -23.330564,
-        -46.723167
+        6,
+        'Indaiá',
+        '116650-80',
+        'Avenida Amazonas, Indaiá, Caraguatatuba, SP',
+        -23.631562,
+        -45.425143
     ),
     (
-        3,
-        'terceira',
-        '116633-40',
-        'Rua Sebastião Carneiro, Jardim Olaria, Caraguatatuba, SP',
-        -23.606752,
-        -45.375374
+        7,
+        'Recanto',
+        '116762-39',
+        'Rua Três, Recanto Som do Mar, Caraguatatuba, SP',
+        -23.600689,
+        -45.365169
     );
 /*!40000 ALTER TABLE `loja_recarga` ENABLE KEYS */
 ;
@@ -528,12 +576,12 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `motorista`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */
 ;
-/*!50503 SET character_set_client = utf8mb4 */
+/*!40101 SET character_set_client = utf8 */
 ;
 
 CREATE TABLE `motorista` (
-    `superUser_id` int unsigned NOT NULL,
-    `cnh` varchar(11) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `superUser_id` int(10) unsigned NOT NULL,
+    `cnh` varchar(11) DEFAULT NULL,
     PRIMARY KEY (`superUser_id`),
     CONSTRAINT `fk_motorista_superUser1` FOREIGN KEY (`superUser_id`) REFERENCES `superuser` (`id`) ON DELETE CASCADE
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
@@ -548,10 +596,7 @@ LOCK TABLES `motorista` WRITE;
 /*!40000 ALTER TABLE `motorista` DISABLE KEYS */
 ;
 
-INSERT INTO
-    `motorista`
-VALUES (4, 'uy444415544'),
-    (5, 'uy444415555');
+INSERT INTO `motorista` VALUES (4, 'uy444415544'), (5, 'uy444415555');
 /*!40000 ALTER TABLE `motorista` ENABLE KEYS */
 ;
 
@@ -564,15 +609,15 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `onibus`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */
 ;
-/*!50503 SET character_set_client = utf8mb4 */
+/*!40101 SET character_set_client = utf8 */
 ;
 
 CREATE TABLE `onibus` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `placa` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `quantidade_passageiros` int DEFAULT NULL,
-    `estado_atual` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `possui_acessibilidade` varchar(3) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `placa` varchar(15) NOT NULL,
+    `quantidade_passageiros` int(11) DEFAULT NULL,
+    `estado_atual` varchar(20) DEFAULT NULL,
+    `possui_acessibilidade` varchar(3) DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB AUTO_INCREMENT = 10 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */
@@ -628,14 +673,14 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `percurso`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */
 ;
-/*!50503 SET character_set_client = utf8mb4 */
+/*!40101 SET character_set_client = utf8 */
 ;
 
 CREATE TABLE `percurso` (
-    `id` int unsigned NOT NULL AUTO_INCREMENT,
-    `ordem_do_percurso` int DEFAULT NULL,
-    `linha_id` int unsigned NOT NULL,
-    `pontoOnibus_id` int NOT NULL,
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `ordem_do_percurso` int(11) DEFAULT NULL,
+    `linha_id` int(10) unsigned NOT NULL,
+    `pontoOnibus_id` int(11) NOT NULL,
     PRIMARY KEY (`id`),
     KEY `fk_rotas_pontoOnibus1_idx` (`pontoOnibus_id`),
     KEY `fk_trajeto_paradas_linha1_idx` (`linha_id`),
@@ -687,13 +732,13 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `ponto_de_onibus`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */
 ;
-/*!50503 SET character_set_client = utf8mb4 */
+/*!40101 SET character_set_client = utf8 */
 ;
 
 CREATE TABLE `ponto_de_onibus` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `endereco` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `cep` varchar(9) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `endereco` varchar(255) DEFAULT NULL,
+    `cep` varchar(9) DEFAULT NULL,
     `lat` decimal(9, 6) DEFAULT NULL,
     `lng` decimal(9, 6) DEFAULT NULL,
     PRIMARY KEY (`id`)
@@ -800,17 +845,17 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `superuser`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */
 ;
-/*!50503 SET character_set_client = utf8mb4 */
+/*!40101 SET character_set_client = utf8 */
 ;
 
 CREATE TABLE `superuser` (
-    `id` int unsigned NOT NULL AUTO_INCREMENT,
-    `email` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `senha` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `nome` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `email` varchar(64) NOT NULL,
+    `senha` varchar(100) DEFAULT NULL,
+    `nome` varchar(100) NOT NULL,
     `nascimento` date NOT NULL,
-    `cpf` varchar(14) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `foto` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `cpf` varchar(14) NOT NULL,
+    `foto` varchar(200) DEFAULT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE = InnoDB AUTO_INCREMENT = 8 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
@@ -884,14 +929,14 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `tipos_de_cartao`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */
 ;
-/*!50503 SET character_set_client = utf8mb4 */
+/*!40101 SET character_set_client = utf8 */
 ;
 
 CREATE TABLE `tipos_de_cartao` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `modalidade` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `modalidade` varchar(45) NOT NULL,
     `tarifa` decimal(6, 2) DEFAULT NULL,
-    `tipos_de_cartaocol` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `tipos_de_cartaocol` varchar(45) DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB AUTO_INCREMENT = 4 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */
@@ -907,7 +952,7 @@ LOCK TABLES `tipos_de_cartao` WRITE;
 
 INSERT INTO
     `tipos_de_cartao`
-VALUES (1, 'comum', 5.00, NULL),
+VALUES (1, 'comum', 4.00, NULL),
     (2, 'estudante', 2.50, NULL),
     (3, 'pcd', 0.00, NULL);
 /*!40000 ALTER TABLE `tipos_de_cartao` ENABLE KEYS */
@@ -922,16 +967,16 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `viagem`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */
 ;
-/*!50503 SET character_set_client = utf8mb4 */
+/*!40101 SET character_set_client = utf8 */
 ;
 
 CREATE TABLE `viagem` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `inicio` datetime DEFAULT CURRENT_TIMESTAMP,
-    `duracao` int DEFAULT NULL,
-    `linhas_id` int unsigned DEFAULT NULL,
-    `onibus_id` int DEFAULT NULL,
-    `motorista_SU_id` int unsigned DEFAULT NULL,
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `inicio` datetime DEFAULT current_timestamp(),
+    `duracao` int(11) DEFAULT NULL,
+    `linhas_id` int(10) unsigned DEFAULT NULL,
+    `onibus_id` int(11) DEFAULT NULL,
+    `motorista_SU_id` int(10) unsigned DEFAULT NULL,
     PRIMARY KEY (`id`),
     KEY `fk_viagem_linhas1_idx` (`linhas_id`),
     KEY `fk_viagem_motorista1_idx` (`motorista_SU_id`),
@@ -939,7 +984,7 @@ CREATE TABLE `viagem` (
     CONSTRAINT `fk_viagem_linhas1` FOREIGN KEY (`linhas_id`) REFERENCES `linhas` (`id`) ON DELETE SET NULL,
     CONSTRAINT `fk_viagem_motorista1` FOREIGN KEY (`motorista_SU_id`) REFERENCES `motorista` (`superUser_id`) ON DELETE SET NULL,
     CONSTRAINT `fk_viagem_onibus1` FOREIGN KEY (`onibus_id`) REFERENCES `onibus` (`id`) ON DELETE SET NULL
-) ENGINE = InnoDB AUTO_INCREMENT = 2 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */
 ;
 
@@ -956,8 +1001,48 @@ INSERT INTO
 VALUES (
         1,
         '2024-11-19 16:00:06',
-        NULL,
+        4724,
         15,
+        1,
+        4
+    ),
+    (
+        2,
+        '2024-11-22 22:47:01',
+        1,
+        16,
+        1,
+        4
+    ),
+    (
+        3,
+        '2024-11-22 22:51:31',
+        1,
+        15,
+        1,
+        4
+    ),
+    (
+        4,
+        '2024-11-22 22:54:07',
+        1,
+        16,
+        1,
+        4
+    ),
+    (
+        5,
+        '2024-11-22 22:56:19',
+        1,
+        16,
+        1,
+        4
+    ),
+    (
+        6,
+        '2024-11-22 22:58:15',
+        1,
+        16,
         1,
         4
     );
@@ -983,4 +1068,4 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */
 ;
 
--- Dump completed on 2024-11-19 17:51:58
+-- Dump completed on 2024-11-23  1:31:48
